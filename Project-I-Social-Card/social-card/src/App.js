baae767df9d7e9e3e20c096a6768c12832e8689b
Andrew Brudnak <brudnak@icloud.com>
2019-05-24 21:32:20 -0700

    Setup Sass for social-card project
    - Ran yarn add node-sass and changed
    Header.css to Header.scss, verified it was
    working correctly by changing the font color

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import HeaderTitle from './components/HeaderComponents/HeaderTitle'
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Social Card!</h3>
+      <HeaderTitle />
+      {/* <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
         few files that we've already included in there to get you started right
@@ -16,9 +18,9 @@ const App = () => {
           Don't forget to `default export` your components and import them here
           inside of this file in order to make them work.
         </strong>
-      </p>
+      </p> */}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
