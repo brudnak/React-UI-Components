9debdc4dc5d6811212ed5327e8c77bf6c3061292
Andrew Brudnak <brudnak@icloud.com>
2019-05-27 13:00:53 -0700

    CardComponents completed
    - The social-card project has now
    met MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,20 @@
-import React from 'react';
-import './Card.css';
+import React from 'react'
+import './Card.scss'
+
+const CardBanner = () => {
+  const REACT_LOGO_PATH =
+    'https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png'
+  return (
+    <div>
+      <a className='CardBanner-anchor' href={'https://reactjs.org/'}>
+        <img
+          className='CardBanner-image'
+          src={REACT_LOGO_PATH}
+          alt={'JavaScript React logo'}
+        />
+      </a>
+    </div>
+  )
+}
+
+export default CardBanner
